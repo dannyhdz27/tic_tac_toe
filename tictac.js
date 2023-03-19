62733f013d36eb18e10a3eaf4806e14571e31033
Daniel H <dannyhdz27@gmail.com>
2023-03-18 21:10:32 -0500

    made reset button. working on input form

--- tictac.js
+++ tictac.js
@@ -1,6 +1,6 @@
 const gameState = {
   playerName: ["x", "o"],
-  inputName: [" "," "]
+  inputName: ["", ""],
   board: [
     [null, null, null],
     [null, null, null],
@@ -21,7 +21,17 @@ for (let i = 0; i < 3; i++) {
   }
 }
 
-// let condition1 = gameState.board[0].toString() === ["X", "X", "X"].toString();
+function updateScore() {
+  let xScore = document.querySelector(".xScore");
+  const xScoreDisplay = document.createElement("span");
+  xScore.appendChild(xScoreDisplay);
+  xScoreDisplay.innerText = gameState.wins[0];
+
+  let oScore = document.querySelector(".oScore");
+  const oScoreDisplay = document.createElement("span");
+  oScore.appendChild(oScoreDisplay);
+  oScoreDisplay.innerText = gameState.wins[1];
+}
 
 function checkWin() {
   let hasWon = false;
@@ -65,9 +75,9 @@ function checkWin() {
   }
   if (hasWon) {
     let playerName = gameState.playerName[gameState.currentPlayeridx];
-    playerText.innerText = `${playerName}'s wins`;
+    playerText.innerText = `${playerName} wins`;
     gameState.wins[gameState.currentPlayeridx]++;
-
+    updateScore();
     resetGame();
   }
 }
@@ -84,6 +94,9 @@ function checkTie() {
   playerText.innerText = "it's a tie!";
 }
 
+const resetButton = document.querySelector(".resetButton");
+resetButton.addEventListener("click", resetGame);
+
 //Reset Game
 function resetGame() {
   gameState.board = [
@@ -91,6 +104,9 @@ function resetGame() {
     [null, null, null],
     [null, null, null],
   ];
+
+  gameState.wins = [0, 0];
+
   const allDivs = document.querySelectorAll("div");
   allDivs.forEach((xClass) => {
     xClass.classList.remove("X");
@@ -106,17 +122,14 @@ function resetGame() {
 }
 
 let player = document.querySelector(".currentPlayer");
-const playerText = document.createElement("span");
+const playerText = document.createElement("p");
 player.appendChild(playerText);
 
 board.addEventListener("click", (event) => {
-  console.log(gameState);
+  // console.log(gameState);
 
   const row = event.target.id[0];
   const column = event.target.id[2];
-  //   const div = document.querySelectorAll("div");
-  //   && !div.classList.contains("X")) ||
-  //   !div.classList.contains("O")
 
   if (gameState.playerName[gameState.currentPlayeridx] === "x") {
     gameState.board[row][column] = "X";
@@ -124,6 +137,7 @@ board.addEventListener("click", (event) => {
     event.target.classList.add("X");
     checkWin();
     checkTie();
+
     gameState.currentPlayeridx = 1;
   } else if (gameState.playerName[gameState.currentPlayeridx] === "o") {
     gameState.board[row][column] = "O";
@@ -131,6 +145,7 @@ board.addEventListener("click", (event) => {
     event.target.classList.add("O");
     checkWin();
     checkTie();
+
     gameState.currentPlayeridx = 0;
   }
 });
